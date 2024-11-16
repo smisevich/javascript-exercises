a1a75dbfd86eeeecda3ac4de79c1f9356198eb59
Stanley Misevich <stanmisevich@gmail.com>
2024-11-15 20:54:29 -0500

    comlete js challenge 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let inputArray = input.split("");
+  inputArray.reverse();
+  let reversedInput = inputArray.join('');
+//   console.log(reversedInput);
+  return reversedInput;
 };
+
+// reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
